ea1a8226e82a0468970878a5c958a02752660c34
PrachiMaurya34 <0494prachimaurya9b@gmail.com>
2024-06-03 10:29:12 +0530

    My First Code on github

---
+++ FactorialCalculator.java
@@ -0,0 +1,18 @@
+public class FactorialCalculator 
+{
+    public static void main(String[] args) 
+	{
+        int number = 5; 
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " = " + factorial);
+    }
+    public static long calculateFactorial(int n) 
+	{
+        if (n == 0 || n == 1) 
+		{
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
